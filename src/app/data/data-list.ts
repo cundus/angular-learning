282bcb457faba7163de02f3e3e8f951d5eadace9
sundusardian <sundusardian@gmail.com>
2022-06-13 14:29:26 +0700

    adding mock data

---
+++ src/app/data/data-list.ts
@@ -0,0 +1,18 @@
+import { IData } from '../models/data.model';
+
+export const LISTDATA: IData[] = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'johndoe@mail.com',
+    phone: '+1-555-555-5555',
+    createdAt: '2020-01-01',
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+    email: 'janedoe@mail.com',
+    phone: '+1-555-555-5555',
+    createdAt: '2020-02-01',
+  },
+];
