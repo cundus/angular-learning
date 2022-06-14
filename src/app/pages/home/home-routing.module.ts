a64d78ce1ed2ec6d61274095ed8cd9c9b3663c46
sundusardian <sundusardian@gmail.com>
2022-06-14 16:52:07 +0700

    adding new route and styling

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
     loadChildren: () =>
       import('../table-list/table-list.module').then((m) => m.TableListModule),
+  },
+  {
+    path: 'add-data',
+    loadChildren: () =>
+      import('../add-data/add-data.module').then((m) => m.AddDataModule),
+  },
+  {
+    path: 'detail-data',
+    loadChildren: () =>
+      import('../detail-data/detail-data.module').then(
+        (m) => m.DetailDataModule
+      ),
+  },
+  {
+    path: 'edit-data',
+    loadChildren: () =>
+      import('../edit-data/edit-data.module').then((m) => m.EditDataModule),
   },
 ];
 
