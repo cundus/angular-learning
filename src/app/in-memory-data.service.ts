b057ad78ab69c5dab9c8f1f3abb44b97e9699c2c
sundusardian <sundusardian@gmail.com>
2022-06-14 16:49:05 +0700

    adding localstorage checking

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -7,36 +7,46 @@ import { IData } from './models/data.model';
 })
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    const data = [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'johndoe@gmail.com',
-        phone: '0123456789',
-        createdAt: '2020-01-01',
-      },
-      {
-        id: 2,
-        name: 'Jane Doe',
-        email: 'janedoe@gmail.com',
-        phone: '0123456789',
-        createdAt: '2021-01-01',
-      },
-      {
-        id: 3,
-        name: 'Jack Doe',
-        email: 'jackdoe@gmail.com',
-        phone: '0123456789',
-        createdAt: '2022-01-01',
-      },
-      {
-        id: 4,
-        name: 'Jill Doe',
-        email: 'jill@mail.com',
-        phone: '0123456789',
-        createdAt: '2023-01-01',
-      },
-    ];
+    let localData = localStorage.getItem('data');
+    let data = null;
+    if (localData) {
+      localData = JSON.parse(localData);
+      data = localData;
+      // console.log(data);
+    } else {
+      data = [
+        {
+          id: 1,
+          name: 'John Doe',
+          email: 'johndoe@gmail.com',
+          phone: '0123456789',
+          createdAt: '2020-01-01',
+        },
+        {
+          id: 2,
+          name: 'Jane Doe',
+          email: 'janedoe@gmail.com',
+          phone: '0123456789',
+          createdAt: '2021-01-01',
+        },
+        {
+          id: 3,
+          name: 'Jack Doe',
+          email: 'jackdoe@gmail.com',
+          phone: '0123456789',
+          createdAt: '2022-01-01',
+        },
+        {
+          id: 4,
+          name: 'Jill Doe',
+          email: 'jill@mail.com',
+          phone: '0123456789',
+          createdAt: '2023-01-01',
+        },
+      ];
+      localStorage.setItem('data', JSON.stringify(data));
+    }
+
     return { data };
   }
 
