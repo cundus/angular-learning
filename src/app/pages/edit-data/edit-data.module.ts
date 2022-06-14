d50ae7285985e36f55b2d15582089002d903bf44
sundusardian <sundusardian@gmail.com>
2022-06-14 16:20:57 +0700

    create Edit Data Component

---
+++ src/app/pages/edit-data/edit-data.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EditDataRoutingModule } from './edit-data-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EditDataRoutingModule
+  ]
+})
+export class EditDataModule { }
