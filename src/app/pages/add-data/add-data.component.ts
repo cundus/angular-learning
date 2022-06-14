0915f17abb90e2e3edc4f989ac8a5d8ee4a66fc8
sundusardian <sundusardian@gmail.com>
2022-06-14 16:50:03 +0700

    done create functionality for add data

--- src/app/pages/add-data/add-data.component.ts
+++ src/app/pages/add-data/add-data.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { DataService } from 'src/app/data.service';
+import { IData } from 'src/app/models/data.model';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-add-data',
   templateUrl: './add-data.component.html',
-  styleUrls: ['./add-data.component.css']
+  styleUrls: ['./add-data.component.css'],
 })
 export class AddDataComponent implements OnInit {
+  name = new FormControl('', [Validators.required]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  phone = new FormControl('', [Validators.required]);
 
-  constructor() { }
+  constructor(private dataService: DataService, private location: Location) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (this.name.invalid || this.email.invalid || this.phone.invalid) {
+      return alert('Please fill all the fields');
+    }
+
+    this.dataService
+      .addData({
+        id: this.generateId(),
+        name: this.name.value,
+        email: this.email.value,
+        phone: this.phone.value,
+        createdAt: this.generateDate(),
+      } as IData)
+      .subscribe((data) => {
+        // console.log(data);
+        this.location.back();
+      });
   }
 
+  generateId() {
+    return Math.floor(Math.random() * 1000000);
+  }
+
+  generateDate() {
+    let date = new Date();
+    return date.toLocaleDateString();
+  }
 }
--- src/app/pages/add-data/add-data.module.ts
+++ src/app/pages/add-data/add-data.module.ts
@@ -3,9 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { AddDataRoutingModule } from './add-data-routing.module';
 import { AddDataComponent } from './add-data.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AddDataComponent],
-  imports: [CommonModule, AddDataRoutingModule],
+  imports: [
+    CommonModule,
+    AddDataRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
 })
 export class AddDataModule {}
