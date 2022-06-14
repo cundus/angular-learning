44dbabc4aead1656ac003240eaabf292e0ea19b1
sundusardian <sundusardian@gmail.com>
2022-06-14 16:51:33 +0700

    done functionality for edit data

--- src/app/pages/edit-data/edit-data.component.ts
+++ src/app/pages/edit-data/edit-data.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { DataService } from 'src/app/data.service';
+import { IData } from 'src/app/models/data.model';
+import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-data',
   templateUrl: './edit-data.component.html',
-  styleUrls: ['./edit-data.component.css']
+  styleUrls: ['./edit-data.component.css'],
 })
 export class EditDataComponent implements OnInit {
+  name = new FormControl('', [Validators.required]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  phone = new FormControl('', [Validators.required]);
+  data!: IData;
 
-  constructor() { }
+  constructor(
+    private dataService: DataService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.dataService.getDataById(+params['id']).subscribe((data) => {
+        this.name.setValue(data.name);
+        this.email.setValue(data.email);
+        this.phone.setValue(data.phone);
+        this.data = data;
+      });
+    });
   }
 
+  onSubmit() {
+    if (this.name.invalid || this.email.invalid || this.phone.invalid) {
+      return alert('Please fill all the fields');
+    }
+
+    // console.log(this.name.value, this.email.value, this.phone.value);
+    // console.log(this.data);
+
+    this.dataService
+      .updateData({
+        name: this.name.value,
+        email: this.email.value,
+        phone: this.phone.value,
+        id: this.data.id,
+        createdAt: this.data.createdAt,
+      })
+      .subscribe(() => {
+        this.router.navigate(['/']);
+      });
+  }
 }
--- src/app/pages/edit-data/edit-data.module.ts
+++ src/app/pages/edit-data/edit-data.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EditDataRoutingModule } from './edit-data-routing.module';
+import { EditDataComponent } from './edit-data.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [],
+  declarations: [EditDataComponent],
   imports: [
     CommonModule,
-    EditDataRoutingModule
-  ]
+    EditDataRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
 })
-export class EditDataModule { }
+export class EditDataModule {}
