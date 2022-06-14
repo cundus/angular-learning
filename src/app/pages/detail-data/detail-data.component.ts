672e5f062e3cfc83c792a3fabb2b1ef54edc90bd
sundusardian <sundusardian@gmail.com>
2022-06-14 16:51:06 +0700

    create Functionality for detail-data

--- src/app/pages/detail-data/detail-data.component.ts
+++ src/app/pages/detail-data/detail-data.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { IData } from '../../models/data.model';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'app-detail-data',
   templateUrl: './detail-data.component.html',
-  styleUrls: ['./detail-data.component.css']
+  styleUrls: ['./detail-data.component.css'],
 })
 export class DetailDataComponent implements OnInit {
+  data: IData = {
+    id: 0,
+    name: '',
+    email: '',
+    phone: '',
+    createdAt: '',
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private dataService: DataService,
+    private location: Location
+  ) {
+    this.route.params.subscribe((params) => {
+      this.dataService.getDataById(+params['id']).subscribe((data) => {
+        this.data = data;
+        // console.log(data);
+      });
+    });
   }
 
+  ngOnInit(): void {}
+
+  onDelete(): void {
+    // console.log(this.data.id);
+
+    if (confirm('Are you sure to delete ' + this.data.name + ' ?')) {
+      this.dataService.deleteData(this.data.id).subscribe(() => {
+        this.location.back();
+      });
+    }
+  }
 }
--- src/app/pages/detail-data/detail-data.module.ts
+++ src/app/pages/detail-data/detail-data.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DetailDataRoutingModule } from './detail-data-routing.module';
 import { DetailDataComponent } from './detail-data.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [DetailDataComponent],
-  imports: [CommonModule, DetailDataRoutingModule],
+  imports: [CommonModule, DetailDataRoutingModule, MatButtonModule],
 })
 export class DetailDataModule {}
